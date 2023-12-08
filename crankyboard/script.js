c890a3cb34e0186624b4cd2b814618501eb06190
rae <114977596+stuffbyrae@users.noreply.github.com>
2023-12-08 10:06:39 -0800

    Updating part I...

--- crankyboard/script.js
+++ crankyboard/script.js
@@ -62,16 +62,16 @@ function set_scoreboard(event, elem) {
   id = elem.id;
   if ($(elem).is("#ww") || $(elem).is("#zp") || $(elem).is("#hm") || $(elem).is("#sn1") || $(elem).is("#sn2") || $(elem).is("#sn3") || $(elem).is("#sn4") || $(elem).is("#sn5")) {
     if (event.shiftKey) {
-      $(".sb_name").html(window[id+"_sim_name"]);
+      $(".sb_name").html('<a href="' + window[id+"_link"] + '">' + window[id+"_sim_name"] + '</a>');
       $(".sb_cat").html(window[id+"_sim_category"]);
       $(".sb_e").html(window[id+"_sim_scores"]);
     } else {
-      $(".sb_name").html(window[id+"_name"]);
+      $(".sb_name").html('<a href="' + window[id+"_link"] + '">' + window[id+"_name"] + '</a>');
       $(".sb_cat").html(window[id+"_category"]);
       $(".sb_e").html(window[id+"_scores"]);
     }
   } else {
-    $(".sb_name").html(window[id+"_name"]);
+    $(".sb_name").html('<a href="' + window[id+"_link"] + '">' + window[id+"_name"] + '</a>');
     $(".sb_cat").html(window[id+"_category"]);
     $(".sb_e").html(window[id+"_scores"]);
   }
@@ -85,27 +85,77 @@ function set_scoreboard(event, elem) {
             var sec = convert((timevalue % 60).toFixed(3));
             $(el).children().children(".v").html("0" + min + ":" + sec.substring(0, sec.length - 1));
         }
-        var oldname = $(el).children().children(".p").html();
-        if (oldname == "rae") {
+        if ($(elem).is("#pr1") || $(elem).is("#pr2") || $(elem).is("#pr3") || $(elem).is("#pr4") || $(elem).is("#pr5") || $(elem).is("#pr6") || $(elem).is("#pr7") || $(elem).is("#pr8")) {
+            numbervalue = $(el).children().children(".v").html();
+            timevalue = (numbervalue);
+            var min = Math.floor(timevalue / 60000);
+            var sec = ((timevalue % 60000) / 1000).toFixed(3);
+            $(el).children().children(".v").html(min + ":" + sec);
+        }
+        if ($(elem).is("#pr1")) {
+            if ($(el).children().children(".v").html() <= "0:34.900") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr2")) {
+            if ($(el).children().children(".v").html() <= "0:33.100") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr3")) {
+            if ($(el).children().children(".v").html() <= "0:29.600") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr4")) {
+            if ($(el).children().children(".v").html() <= "0:38.200") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr5")) {
+            if ($(el).children().children(".v").html() <= "0:37.200") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr6")) {
+            if ($(el).children().children(".v").html() <= "0:37.300") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr7")) {
+            if ($(el).children().children(".v").html() <= "0:35.400") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#pr8")) {
+            if ($(el).children().children(".v").html() <= "0:40.400") {
+              $(el).children().children(".v").append("<i class='goal fa-solid fa-circle-check'></i>");
+            }
+        }
+        if ($(elem).is("#dy")) {
+            $(el).children().children(".v").append("m");
+        }
+        
+        var oldname = $(el).children().children(".p").html() + " ";
+        if (oldname.startsWith("rae ")) {
             oldname += "<img class='badge badge_rae' title='Crankyboard Admin' />";
-            $(el).children().children(".p").html(oldname);            
         }
-        if (oldname == "GantProdux") {
-            oldname += "<img class='badge badge_tym' title='Tiny Yellow Machine' />";
-            $(el).children().children(".p").html(oldname);            
+        if (oldname.startsWith("GantProdux ")) {
+            oldname += "<img class='badge badge_tym' title='TYM - Host of Tiny Yellow Machine.' />";
         }
-        if (oldname == "andy" || oldname == "dave" || oldname == "marc" || oldname == "Arisa") {
-            oldname += "<img class='badge badge_panic' title='Panic Team' />";
-            $(el).children().children(".p").html(oldname);            
+        if (oldname.startsWith("andy ") || oldname.startsWith("dave ") || oldname.startsWith("morc ") || oldname.startsWith("Arisa ")) {
+            oldname += "<img class='badge badge_panic' title='Panic Team - An employee from Panic, Inc.' />";
         }
-        if (oldname == "bennett" || oldname == "Giles" || oldname == "mistermo" || oldname == "davemakes" || oldname == "7635678961621917") {
-            oldname += "<img class='badge badge_s1' title='Season 1 Dev' />";
-            $(el).children().children(".p").html(oldname);            
+        if (oldname.startsWith("bennett ") || oldname.startsWith("Giles ") || oldname.startsWith("mistermo ") || oldname.startsWith("davemakes ") || oldname.startsWith("7635678961621917 ")) {
+            oldname += "<img class='badge badge_s1' title='Season 1 Dev - Has made a game that was a part of Season 1.' />";
         }
-        if (oldname == "davemakes" || oldname == "LedbetterGames" || oldname == "RDK") {
-            oldname += "<img class='badge badge_catalog' title='Catalog Dev' />";
-            $(el).children().children(".p").html(oldname);            
+        if (oldname.startsWith("davemakes ") || oldname.startsWith("LedbetterGames ") || oldname.startsWith("RDK ") || oldname.startsWith("ScenicRouteSoftware ") || oldname.startsWith("LDBR_art ") || oldname.startsWith("stevelavietes ") || oldname.startsWith("Link ") || oldname.startsWith("RPDev ") || oldname.startsWith("SquidGod ") || oldname.startsWith("garethiw ") || oldname.startsWith("vogelscript ") || oldname.startsWith("Pichuscute ") || oldname.startsWith("MouflonCloud ") || oldname.startsWith("burgess ") || oldname.startsWith("Toad ")) {
+            oldname += "<img class='badge badge_catalog' title='Catalog Dev - Has made a game that has been published on Catalog.' />";
         }
+        if (oldname.startsWith("tavi ") || oldname.startsWith("uncletrunks ") || oldname.startsWith("2257259091194867 ") || oldname.startsWith("rae ")) {
+            oldname += "<img class='badge badge_dominator' title='Dominator - Has taken all #1 ranks in a multi-category game.' />";
+        }
+        $(el).children().children(".p").html(oldname);
     });
     if ($(".sb_e").html() == "") {
       $(".sb_e").html("<p class='sb_empty'>This scoreboard's currently empty. Start playing and take Rank 1!</p>")
